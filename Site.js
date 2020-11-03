aa6d683f99d92908f47f38de285eecae26287cd3
mattmax1 <matthieukoebsch@hotmail.com>
2020-11-03 14:10:10 +0100

    Enfin

--- Site.js
+++ Site.js
@@ -1,1 +1,26 @@
-let top1= document.getElementById ('top1')
+
+function toggleImagetop1() {
+    var img1 = "IMG/medal1.png";
+    var img2 = "IMG/oui.png";
+    var imgElement = document.getElementById('toggleImagetop1');
+ 
+    imgElement.src = (imgElement.src === img1)? img2 : img1;
+
+ }
+
+ function toggleImagetop2() {
+    var img1 = "IMG/medal2.png";
+    var img2 = "IMG/oui.png";
+    var imgElement = document.getElementById('toggleImagetop2');
+ 
+    imgElement.src = (imgElement.src === img1)? img2 : img1;
+
+ }
+ function toggleImagetop3() {
+    var img1 = "IMG/medal3.png";
+    var img2 = "IMG/oui.png";
+    var imgElement = document.getElementById('toggleImagetop3');
+ 
+    imgElement.src = (imgElement.src === img1)? img2 : img1;
+
+ }
