aba731c14135a19abe4bb3b86fc6f9898c877b66
Brendan <brendanbx@gmail.com>
2020-11-03 14:16:02 +0100

    un nom

--- Site.js
+++ Site.js
@@ -1,1 +1,9 @@
-let top1= document.getElementById ('top1')
+function toggleImage() {
+    var img1 = "IMG/TOP1.png";
+    var img2 = "IMG/oui.png";
+    
+    var imgElement = document.getElementById('toggleImage');
+ 
+    imgElement.src = (imgElement.src === img1)? img2 : img1;
+
+ }
